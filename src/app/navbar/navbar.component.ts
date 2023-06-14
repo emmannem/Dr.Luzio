fa2c488c2599f9d497c0d3ef7d206acad6ab808d
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-06-14 02:17:10 -0600

    add funcion-salir

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../service/localStorage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+  constructor(
+    private cache: LocalStorageService,
+  ) { }
+
+  cerrarSesion() {
+    this.cache.clear();
+  }
 }
--- src/app/service/localStorage.service.ts
+++ src/app/service/localStorage.service.ts
@@ -4,31 +4,21 @@ import { Injectable, HostListener } from '@angular/core';
     providedIn: 'root'
 })
 export class LocalStorageService {
-
-    private isPageClosed = true;
     private key = 'matricula';
-    @HostListener('window:beforeunload')
-    onBeforeUnload() {
-        this.isPageClosed = true;
-        localStorage.removeItem(this.key); // Cambia 'miItem' por el nombre del ítem que deseas eliminar
-    }
 
-    constructor() {
-        this.isPageClosed = false;
-    }
+    constructor() { }
 
-    public getItem(): any {
-        if (this.isPageClosed) {
-            return null;
-        }
+    getItem() {
         return localStorage.getItem(this.key);
     }
 
-    public setItem(value: any): void {
-        if (!this.isPageClosed) {
-            localStorage.setItem(this.key, value);
-            // console.log(localStorage.getItem('matricula'))
-        }
+    setItem(value: any) {
+        localStorage.setItem(this.key, value);
+        // console.log(localStorage.getItem('matricula'))
+    }
+
+    clear() {
+        localStorage.removeItem(this.key);
     }
 
 }
