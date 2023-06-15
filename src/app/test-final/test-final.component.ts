4685781622d528465d733b100426a40f684d7813
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-06-15 10:24:01 -0600

    add text-test-Final

--- src/app/test-final/test-final.component.ts
+++ src/app/test-final/test-final.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-test-final',
   templateUrl: './test-final.component.html',
   styleUrls: ['./test-final.component.css']
 })
 export class TestFinalComponent {
+  constructor(
+  ) { }
+
 
 }
+
+
