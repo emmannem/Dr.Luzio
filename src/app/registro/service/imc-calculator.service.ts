e9f6fdf4434fba2d570d9de1115932a021424f6a
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-06-15 03:37:48 -0600

    fix calculo-imc

--- src/app/registro/service/imc-calculator.service.ts
+++ src/app/registro/service/imc-calculator.service.ts
@@ -11,12 +11,12 @@ export class IMCcalculatorService {
     imcClassification(peso: number, estatura: number, edad: number, sexo: string) {
 
 
-        const imc : number = this.calculateIMC(peso,estatura);
+        const imc: number = this.calculateIMC(peso, estatura);
         let classification = '';
 
         // Clasificación según la OMS
         if (imc < 18.5) {
-            classification = 'Bajo peso';
+            classification = 'Bajo';
         } else if (imc >= 18.5 && imc < 25) {
             classification = 'Normal';
         } else if (imc >= 25 && imc < 30) {
@@ -29,7 +29,7 @@ export class IMCcalculatorService {
         if (sexo === 'M') {
             if (edad && edad < 18 && imc < 18.5) {
                 if (imc <= 16.5) {
-                    classification = 'Bajo peso';
+                    classification = 'Bajo';
                 } else if (imc >= 18.5 && imc < 25) {
                     classification = 'Normal';
                 } else if (imc >= 25 && imc < 30) {
@@ -39,7 +39,7 @@ export class IMCcalculatorService {
                 }
             } else {
                 if (imc < 20) {
-                    classification = 'Bajo peso';
+                    classification = 'Bajo';
                 } else if (imc >= 20 && imc < 25) {
                     classification = 'Normal';
                 } else if (imc >= 25 && imc < 30) {
@@ -51,7 +51,7 @@ export class IMCcalculatorService {
         } else if (sexo === 'F') {
             if (edad && edad < 18) {
                 if (imc <= 16 && imc < 17.5) {
-                    classification = 'Bajo peso';
+                    classification = 'Bajo';
                 } else if (imc >= 17.5 && imc < 25) {
                     classification = 'Normal';
                 } else if (imc >= 25 && imc < 30) {
@@ -61,7 +61,7 @@ export class IMCcalculatorService {
                 }
             } else {
                 if (imc < 19) {
-                    classification = 'Bajo peso';
+                    classification = 'Bajo';
                 } else if (imc >= 19 && imc < 24) {
                     classification = 'Normal';
                 } else if (imc >= 24 && imc < 30) {
@@ -75,7 +75,7 @@ export class IMCcalculatorService {
         return classification;
     }
 
-    private calculateIMC(peso: number, estatura: number) : number {
+    private calculateIMC(peso: number, estatura: number): number {
         if (peso && estatura) {
             const height = estatura / 100;
             return peso / (height * height);
